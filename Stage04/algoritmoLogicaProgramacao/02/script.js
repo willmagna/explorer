ad2ddc9a8b00d09838a9e8ed0d0542eacf1b10dc
William Lodea Magnabosco <williamlodea@gmail.com>
2022-11-03 16:03:08 -0400

    a

---
+++ Stage04/algoritmoLogicaProgramacao/02/script.js
@@ -0,0 +1,12 @@
+/**
+ * solicite 2 números, faça a soma deles
+ * e apresente o resultado final ao usuário
+ */
+
+alert("Iremos somar 2 números");
+let numberOne = Number(prompt("Digite o primeiro número:"));
+let numberTwo = Number(prompt("Digite o segundo número"));
+
+let result = numberOne + numberTwo;
+
+alert(`A soma total é de ${result}`);
