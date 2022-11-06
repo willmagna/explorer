5792a130c1e01be0788d5bbcb26bb4d33d3ba9c0
William Lodea Magnabosco <williamlodea@gmail.com>
2022-11-05 23:58:05 -0400

    Terminando o projeto do jogo de adivinhação

--- Stage05/jogo-adivinhacao/main.js
+++ Stage05/jogo-adivinhacao/main.js
@@ -1,9 +1,25 @@
 let randomNumber = Math.floor(Math.random() * 11);
 let count = 0;
 
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+
 console.log(`Ramdom ${randomNumber}`);
 
-function handleClick(event){
+
+//A moral de passar uma função como parâmetro é, só depois que 
+//a pessoa clicar é que a função que foi como parâmentro vai ser executada.
+//aqui ta adicionando tbm o onclick nos elementos porque é boa práatica
+//não colocar eventos de JS no HTML
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', playAgain);
+document.addEventListener('keydown', handleKeyDown);
+
+
+function handleTryClick(event){
 
   event.preventDefault();
 
@@ -20,29 +36,40 @@ function handleClick(event){
 
   if(inputNumber === randomNumber){
 
-    document.querySelector(".screen1").classList.toggle("hide");
-    document.querySelector(".screen2").classList.toggle("hide");
+    toggleScreen();
+/*    document.querySelector(".screen2 h2").innerText = `Acertou depois de ${count} ${count > 1 ? "tentativas." : "tentativa."}`;*/
 
-    document.querySelector(".screen2 h2").innerText = `Acertou depois de ${count} ${count > 1 ? "Tentativas" : "Tentativa"}`;
+    screen2.querySelector("h2").innerText = `Acertou depois de ${count} ${count > 1 ? "tentativas." : "tentativa."}`;
+
+    //na variável screen2 foi atrabuida exatamente o elemento screen2, ou seja, não precisa mais acessar o .screen2 h2. Acessa direto o elemento dentro do nó .screen2.
 
   }else{
     alert(`Errouuuuuuu, tente novamente.`);
   }
   
+  document.querySelector("#inputNumber").value = "";
   console.log(`ramdom ${randomNumber}`);
 
 }
 
 function playAgain() {
 
-  document.querySelector(".screen1").classList.toggle("hide");
-  document.querySelector(".screen2").classList.toggle("hide");
+  toggleScreen();
 
-  randomNumber = Math.floor(Math.random() * 11);
   count = 0;
-
+  randomNumber = Math.floor(Math.random() * 11);
   document.querySelector("#inputNumber").value = "";
 
 }
 
+function toggleScreen(){
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function handleKeyDown(event){
+  if(event.key == 'Enter' && screen1.classList.contains('hide')){
+    playAgain();
+  }
+}
 
--- arquivos/Anotacoes.md
+++ arquivos/Anotacoes.md
@@ -348,3 +348,20 @@ Podemos alterar estilos ou adicionar classes em elementos
 
 # Input
 * Todo o input pega o valor através do value
+
+# Event-driven
+A DOM é direcionada a eventos event-drive. Significa que ela poderá reagir a qualquer tipo de evento relacionado à página.
+
+* Podemos entender em 2 fases
+  - A ocorrência do evento e reação à ocorrência
+
+1. Eventos
+  Ações que acontecem na página, site ou aplicação
+  * Carregamento do documento, aparição de elementos na tela
+  * Modificação de propriedades da página (largura, altura, scroll)
+  * Cliques de mouse e digitação do teclado
+  * Interação com sons, imagens, vídeos
+  * Outros
+
+2. Reações
+  O sistema poderá executar reações às ações. Executar uma função sempre que determinada ação acontecer.
