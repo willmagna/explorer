05e48e8c053a9e383b6e1ad7451fc2d1497719c0
William Lodea Magnabosco <williamlodea@gmail.com>
2022-11-09 14:53:50 -0400

    Até aqui o counter ta funcionando

--- Stage05/FocusTimer2.0/js/index.js
+++ Stage05/FocusTimer2.0/js/index.js
@@ -10,29 +10,100 @@ const buttonRain = document.querySelector('.rain');
 const buttonCoffeeShop = document.querySelector('.coffeeShop');
 const buttonFirePlace = document.querySelector('.firePlace');
 
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
 
-buttonPlay.addEventListener('click', () => {
+let timerTimeOut;
+let minutes = Number(minutesDisplay.textContent);
+
+
+function countDown(){
+
+  timerTimeOut = setTimeout(() => {
+
+    let seconds = Number(secondsDisplay.textContent);
+    let localMinutes = Number(minutesDisplay.textContent);
+    let isFinished = localMinutes <= 0 && seconds <= 0;
+
+    if(isFinished){
+      resetTimer(minutes, 0);
+      return;
+    }
+
+    if(seconds <= 0){
+      seconds = 5;
+      --localMinutes;
+    }
+
+    minutesDisplay.textContent = String(localMinutes).padStart(2, "0");
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+
+    countDown();
+    
+  }, 1000);
+
+}
+
+function resetTimer(newMinutes, seconds){
+  minutesDisplay.textContent = String(newMinutes).padStart(2 , "0");
+  secondsDisplay.textContent = String(seconds).padStart(2 , "0");
+  resetControls();
+  holdTimer();
+}
+
+function resetControls(){
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+}
+
+function playtimer(){
   buttonPlay.classList.add('hide');
   buttonPause.classList.remove('hide');
+}
+
+function holdTimer(){
+  clearTimeout(timerTimeOut);
+}
+
+function startTimer(){
+  playtimer();
+  countDown();
+}
+
+
+buttonPlay.addEventListener('click', () => {
+  startTimer();
 })
 
 
 buttonPause.addEventListener('click', () => {
-  alert('pause');
+  resetControls();
+  holdTimer();
 })
 
 buttonStop.addEventListener('click', () => {
-  alert('stop');
+  resetTimer(minutes, 0);
 })
 
 buttonIncrease.addEventListener('click', () => {
-  alert('Increase');
+
+  let minutes = Number(minutesDisplay.textContent);
+  minutesDisplay.textContent = String(minutes + 5).padStart(2, "0");
 })
 
 buttonDecrease.addEventListener('click', () => {
-  alert('Descrease');
+
+  let resMinutes = Number(minutesDisplay.textContent) - 5;
+
+  if(resMinutes <= 0) {
+    minutesDisplay.textContent = String(0).padStart(2, "0");
+    return;
+  }
+  minutesDisplay.textContent = String(resMinutes).padStart(2, "0");
 })
 
+
+/*
 buttonForest.addEventListener('click', () => {  
 
   buttonForest.querySelector('svg :nth-child(1)').classList.add('bgButtonSoundsClicked');
@@ -52,3 +123,5 @@ buttonCoffeeShop.addEventListener('click', () => {
 buttonFirePlace.addEventListener('click', () => {
   alert('FirePlace');
 })
+
+*/
