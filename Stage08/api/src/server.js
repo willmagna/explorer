cf9d822359d26de404bd779de76ded328c46a159
William <williamlodea@gmail.com>
2022-11-20 16:00:56 -0400

    Primeiro commit do domingo

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello, world!");
+//Route Params : 
+//No route os parametros são obrigatórios. Ele faz parte do endereço.
+//Chamada: http://localhost:3333/message/5/Will
+
+app.get("/message/:id/:user", (request, response) => {
+
+  const { id, user } = request.params; //desestruturar
+
+  response.send(`
+  Mensagem ID: ${id}. <br/>
+  Para o usuário: ${user}
+  `);
+
+});
+
+//Query params
+//No query os parametros não são obrigatórios
+//Chamada: http://localhost:3333/users?page=1&limit=123
+
+app.get("/users", (request, response) => {
+
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+
 });
 
 const PORT = 3333;
