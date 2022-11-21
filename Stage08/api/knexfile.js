af45f40638f3db583ab15c064f3b172bfb7f819c
William <williamlodea@gmail.com>
2022-11-21 18:06:44 -0400

    Migrations para Link e tag

--- Stage08/api/knexfile.js
+++ Stage08/api/knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb) //para deletar em cascata
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
