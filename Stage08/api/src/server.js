b2b9dd1e908effba35ad3cdfed017ed2603a3f7a
William <williamlodea@gmail.com>
2022-11-20 23:15:34 -0400

    Organizando a estrutura do projeto

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const app = express();
 
+const routes = require("./routes/index.js");
+
+
+const app = express();
 app.use(express.json());
 
-app.post("/users", (request, response) => {
-
-  const { name, email, password } = request.body;
-
-  response.json( { name, email, password } );
-
-
-});
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
