c3677e373d3f935d91c7d05c1075072107c8ea48
William <williamlodea@gmail.com>
2022-11-26 14:11:30 -0400

    first commit of saturday

--- Stage09/rocketnotes/src/components/Header/index.jsx
+++ Stage09/rocketnotes/src/components/Header/index.jsx
@@ -1,11 +1,25 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header(){
 
   return(
     <Container>
+
+        <Profile>
+
+          <img 
+            src="https://avatars.githubusercontent.com/u/18670313?v=4"
+            alt="Texto alternativo" 
+            />
+
+            <div>
+              <span>Bem-vindo</span>
+              <strong>William Magnabosco</strong>
+            </div>
+
+        </Profile>
       
     </Container>
-  );
+  )
 
 }
