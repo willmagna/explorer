463f3f618a6c6af61b83ad8aa011150f8bef1276
William <williamlodea@gmail.com>
2022-11-27 00:25:35 -0400

    Commitão da madrugada

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {ThemeProvider} from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
-import {Details} from './pages/Details';
+
+
+//import {Details} from './pages/Details';
+import {Home} from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>
 )
