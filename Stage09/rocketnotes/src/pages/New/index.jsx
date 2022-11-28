77c9cebddc0df425b68d5f8f524e55fedcbccae7
William <williamlodea@gmail.com>
2022-11-28 15:38:58 -0400

    First commit of Monday. Finalizando o New

--- Stage09/rocketnotes/src/pages/New/index.jsx
+++ Stage09/rocketnotes/src/pages/New/index.jsx
@@ -1,7 +1,11 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 import { Container, Form } from "./styles";
+
 
 export function New(){
   return(
@@ -9,7 +13,6 @@ export function New(){
       <Header />
 
       <main>
-
         <Form>
 
           <header>
@@ -17,16 +20,23 @@ export function New(){
             <a href="/">Voltar</a>
           </header>
 
-          <Input
-            placeholder="Título"
-          />
+          <Input placeholder="Título" />
 
-          <Textarea 
-            placeholder="Observações" 
-          />
+          <Textarea placeholder="Observações" />
 
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="Nova Tag"/>
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
-
       </main>
 
     </Container>
--- Stage09/rocketnotes/src/pages/New/styles.js
+++ Stage09/rocketnotes/src/pages/New/styles.js
@@ -10,6 +10,16 @@ export const Container = styled.div`
   "header"
   "content";
 
+  > main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
+  .tags {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 
 `;
 
