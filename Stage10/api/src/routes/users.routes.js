3073133ad44b860c5d3fbf9a633a04b3147f247a
William <williamlodea@gmail.com>
2022-12-02 12:52:26 -0400

    Carregando imagens e fixing bugs

--- Stage10/api/src/routes/users.routes.js
+++ Stage10/api/src/routes/users.routes.js
@@ -1,14 +1,21 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersControllers = require("../controllers/UsersController.js");
-
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersControllers();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  response.json();
+}); 
+
 
 module.exports = usersRoutes;
