2012cd117aacf8dd5cc3d79c3743debf723288ad
William <williamlodea@gmail.com>
2022-12-05 21:44:46 -0400

    Carregando dados do usuário

--- Stage10/rocketnotes/src/pages/Profile/index.jsx
+++ Stage10/rocketnotes/src/pages/Profile/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Container, Form, Avatar } from "./styles";
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
 
 
 export function Profile(){
+
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState("");
+  const [passwordNew, setPasswordNew] = useState("");
+
+
   return(
     <Container>
       <header>
@@ -36,24 +47,30 @@ export function Profile(){
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={ e => setName(e.target.value) }
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={ e => setEmail(e.target.value) }
         />
 
         <Input 
           placeholder="Senha Atual"
           type="password"
           icon={FiLock}
+          onChange={ e => setPasswordOld(e.target.value) }
         />
 
         <Input 
           placeholder="Nova Senha"
           type="password"
           icon={FiLock}
+          onChange={ e => setPasswordNew(e.target.value) }
         />
 
         <Button title="Salvar" />
@@ -62,4 +79,5 @@ export function Profile(){
 
     </Container>
   );
+
 }
