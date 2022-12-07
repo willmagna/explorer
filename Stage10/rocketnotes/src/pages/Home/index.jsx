1792c09d768425725b59cd2ae75b34eafa617caf
William <williamlodea@gmail.com>
2022-12-07 09:59:12 -0400

    first commit of wednesday - Abrir notas cadastradas

--- Stage10/rocketnotes/src/pages/Home/index.jsx
+++ Stage10/rocketnotes/src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { api } from '../../../../api/src/services/api';
+import { useNavigate } from 'react-router-dom';
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import { Header } from '../../components/Header';
@@ -8,6 +11,58 @@ import { Note } from '../../components/Note';
 
 
 export function Home(){
+
+  const [search, setSearch] = useState("");
+  const [notes , setNotes] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
+
+  const navigate = useNavigate();
+
+  function handleTagSelected(tagName){
+
+    if(tagName === "all"){
+      return setTagsSelected([]);
+    }
+
+    const alreadySelected = tagsSelected.includes(tagName);
+
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    }else{
+      setTagsSelected(prevState => [...prevState, tagName]);
+    }
+
+  }
+
+  function handleDetails(id){
+    navigate(`/details/${id}`);
+  }
+
+  useEffect(() => {
+
+    async function fetchTags(){
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+
+  }, []);
+
+  
+  useEffect(() => {
+
+    async function fetchNotes(){ 
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+      setNotes(response.data.notesWithTags);
+    }
+    
+    fetchNotes();
+
+  }, [tagsSelected, search]);
+
   return(
 
     <Container>
@@ -19,24 +74,47 @@ export function Home(){
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Node" /></li>
+
+        <li>
+          <ButtonText 
+            title="Todos" 
+            onClick={() => handleTagSelected("all")} 
+            isActive={tagsSelected.length === 0}
+          />
+        </li>
+
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name} 
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))
+        }
+
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título"/>
-      </Search>
+        <Input 
+          placeholder="Pesquisar pelo título"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Search> 
 
       <Content>
         <Section title="Minhas notas">
-          <Note data={{
-            title: 'React',
-            tags: [
-              {id: '1', name: 'react'},
-              {id: '2', name: 'node'}
-            ]
-          }}/>
+          {
+            notes.map( (note) => (
+              <Note
+                key={String(note.id)}
+                data={note}
+                onClick={() => handleDetails(note.id)}
+              />
+            ))
+          }
         </Section>
       </Content>
 
--- Stage10/rocketnotes/src/pages/New/index.jsx
+++ Stage10/rocketnotes/src/pages/New/index.jsx
@@ -7,15 +7,22 @@ import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
 import { Container, Form } from "./styles";
 import { useState } from 'react';
+import { api } from '../../../../api/src/services/api';
+import { useNavigate } from 'react-router-dom';
 
 
 export function New(){
 
-  const [links, setLinks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const [newLink, setNewLink] = useState("");
+  const [links, setLinks] = useState([]);
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddLink(){
     setLinks(prevState => [...prevState, newLink]);
@@ -35,6 +42,31 @@ export function New(){
     setTags(prevState => prevState.filter(tag => tag !== deleted));
   }
 
+  async function handleNewNote(){
+
+    if(!title){
+      return alert("Digite o título da nota");
+    }
+
+    if(newLink){
+      return alert("Você deixou uma link no campo para adicionar, mas não clicou em adicionar");
+    }
+  
+    if(newTag){
+      return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar");
+    }
+
+    await api.post("/notes", {
+      title, 
+      description,
+      tags,
+      links
+    });
+
+    alert("Nota criada com sucesso!");
+    navigate("/");
+  }
+
   return(
     <Container>
       <Header />
@@ -47,9 +79,15 @@ export function New(){
             <Link to="/">Voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
+          <Input 
+            placeholder="Título" 
+            onChange={e => setTitle(e.target.value)}  
+          />
 
-          <Textarea placeholder="Observações" />
+          <Textarea 
+            placeholder="Observações" 
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
 
@@ -96,7 +134,10 @@ export function New(){
 
             </div>
           </Section>
-          <Button title="Salvar" />
+          <Button 
+            title="Salvar"
+            onClick={handleNewNote}
+          />
         </Form>
       </main>
 
