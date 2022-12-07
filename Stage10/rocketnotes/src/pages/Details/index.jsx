9322d18d9cd31e067fd664c084caa286821fd64b
William <williamlodea@gmail.com>
2022-12-07 10:19:01 -0400

    Carregando detalhe da nota

--- Stage10/rocketnotes/src/pages/Details/index.jsx
+++ Stage10/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
 import { Container, Links, Content } from './styles';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { api } from '../../../../api/src/services/api';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -6,48 +10,91 @@ import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
 import { ButtonText } from '../../components/ButtonText';
 
+
 export function Details() {
+
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+
+  const navigate = useNavigate();
+
+  function handleBack(){
+    navigate("/");
+  }
+
+  useEffect(()=> {
+
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+
+  }, []);
 
   return(
     <Container>
 
       <Header/>
 
-      <main>
+      {
+
+        data && 
+
+        <main>
         <Content>
           
           <ButtonText title="Excluir nota" />
 
             <h1>
-              Introdução ao React
+              {data.title}
             </h1>
 
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse necessitatibus numquam reprehenderit, consequatur modi unde architecto, culpa amet hic odit laborum harum natus eum, sequi voluptatum nesciunt nulla explicabo! Deserunt!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quos voluptates odio aspernatur magnam odit, eaque, molestiae eos distinctio aperiam qui vitae aliquam rem. Dolor impedit ullam aut repellat? Repellendus?
+              {data.description}
             </p>
 
-            <Section title="Links úteis">
-              <Links>
-               <li>
-                 <a href="#">Item 1</a>
-               </li>
-               <li>
-                 <a href="#">Item 2</a>
-               </li>
-              </Links>
-            </Section>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target="_blank">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+               </Links>
+              </Section>
+            }
 
-            <Section title="Marcadores">
-             <Tag title="express"/>
-             <Tag title="note"/>
-           </Section>
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag 
+                      key={String(tag.id)}
+                      title={tag.name}
+                    />
+                  ))
+                }
+              </Section>
+            }
 
-          <Button title="Voltar" />
+          <Button 
+            title="Voltar" 
+            onClick={handleBack}
+          />
 
         </Content>
-      </main>
-      
+        </main>
+      }
 
     </Container>
   );
